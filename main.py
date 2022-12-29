676ea4695a51893671d5db0fed8237c93d01791c
Mohamed Gomea <mohamed.gouda7@gmail.com>
2022-12-29 10:41:57 +0200

    s
    s

---
+++ main.py
@@ -0,0 +1,10 @@
+import live_view
+from unittest import main
+
+# Test your function by calling it here
+live_view.draw_line_plot()
+live_view.draw_bar_plot()
+live_view.draw_box_plot()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
